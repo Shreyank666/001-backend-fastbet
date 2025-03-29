6790da568ee0b4680ab36a345822758c5649d8ca
Shreyank666 <101159535+Shreyank666@users.noreply.github.com>
2025-03-29 22:13:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -625,6 +625,6 @@ io.on("connection", (socket) => {
 
 
 const PORT = process.env.PORT || 5000;
-server.listen(process.env.PORT || 5000, () => {
-  console.log('Server started on port 5000');
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server started on port ${PORT}`);
 });
