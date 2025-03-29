96e22dee236f8cca0ba366044805511cbf41006b
Shreyank666 <101159535+Shreyank666@users.noreply.github.com>
2025-03-29 22:11:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,21 +41,21 @@ const io = socketIo(server, {
 });
 
 
-// port = 5000
-// CORS configuration
+port = 5000
+CORS configuration
 
-// app.use(
-//   cors({
-//     origin: ["https://dynexbet-front.vercel.app", "https://dynexbet-admin.vercel.app"], // Replace '*' with the specific origin(s) you want to allow, e.g., 'https://yourdomain.com'
-//     methods: ['POST', 'GET', 'PUT', 'DELETE'], // Define allowed HTTP methods
-//     credentials: true, // Allow credentials like cookies to be sent
-//   })
-// );
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://www.98fastbet.com", "https://admin.98fastbet.com"], // Replace '*' with the specific origin(s) you want to allow, e.g., 'https://yourdomain.com'
+    methods: ['POST', 'GET', 'PUT', 'DELETE'], // Define allowed HTTP methods
+    credentials: true, // Allow credentials like cookies to be sent
+  })
+);
+// app.use(cors());
 
 const MONGO_URI = process.env.mongodb_url;   
 // MongoDB connection
-mongoose.connect(`mongodb://localhost:27017/fastbet`)
+mongoose.connect(`mongodb+srv://siddharthojha421:yPlKyyZpefa9Y5lJ@cluster0.rtjwl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
   .then(() => console.log("MongoDB Connected Successfully!"))
   .catch(err => console.error("MongoDB Connection Error:", err));
 
